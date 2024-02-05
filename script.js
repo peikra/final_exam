217caa3a8f17915ee3fa1426be84b62a7e22688a
peikra <mikheil.peikrishvili.1@btu.edu.ge>
2024-02-05 16:20:28 +0400

    popup

--- script.js
+++ script.js
@@ -42,6 +42,8 @@ function getall_products(){
         
 
         function get_items(element){
+
+
             let items_div = document.createElement("div")
             items_div.classList.add("grid_items")
             const container = document.querySelector(".grid_container")
@@ -54,9 +56,21 @@ function getall_products(){
             for_images.append(img)
             let image = element.images[0]
             img.src = image
+           
             let for_titles = document.createElement("div")
             for_titles.classList.add("for_titles")
             items_div.append(for_titles)
+            let for_stars = document.createElement("div")
+
+            for_stars.classList.add("for_stars")
+            for_titles.append(for_stars)
+            
+            for (let i = 0; i < Math.ceil(element.rating); i++){
+                let star = document.createElement("img")
+                star.classList.add("star_img")
+                for_stars.append(star)
+                star.src = "./images/star-solid.svg"
+            }
             let text = document.createElement("h2")
             text.classList.add("product_title")
             for_titles.append(text)
@@ -81,6 +95,19 @@ function getall_products(){
             for_images.append(cart_button)
             cart_button.innerHTML = "Add to cart"
 
+           
+
+            items_div.addEventListener("click", ()=>{
+                let popup = document.querySelector(".popup")
+                popup.style.display = "block"
+                
+                
+                current_post(element.id)
+              
+            })
+
+
+
             let toolbar2 = document.querySelector(".img2")
 
             let toolbar1 = document.querySelector(".img1")
@@ -89,33 +116,34 @@ function getall_products(){
             let paragraph = document.createElement("p")
             let new_button = document.createElement("button")
             let y = 0
+            
             toolbar2.addEventListener("click", ()=>{
-                y=1
+                 y=1
                 if (x===1){
 
-                    
+                        
                     container.style.gridTemplateColumns = "auto"
                     items_div.style.display = 'flex'
                     for_titles.style.marginLeft = "300px"
                     for_titles.style.marginTop = "20px"
                     second_price.style.display = "none"
                     text.style.width = "150px"
-
-                    
+    
+                        
                     paragraph.classList.add("paragr")
                     for_titles.append(paragraph)
                     paragraph.innerHTML = element.description
-
+    
                     cart_button.style.display = 'none'
-                    
-                    
+                        
+                        
                     new_button.classList.add("new_butt")
                     for_titles.append(new_button)
                     new_button.innerHTML = "Add to Cart"
                     x=x+1
-                    
+                        
             }
-
+    
             else if (x===2){
                 container.style.gridTemplateColumns = "auto"
                 items_div.style.display = 'flex'
@@ -124,14 +152,25 @@ function getall_products(){
                 new_button.style.display = 'block'
                 second_price.style.display = "none"
                 paragraph.style.display = 'block'
-
-            }
-            })
+    
+             }
+         })
+            
+            
+            
 
             toolbar1.addEventListener("click", ()=> {
                 if(y===1){
 
-                
+                    items_div.addEventListener("mouseenter", ()=>{
+                        cart_button.style.display = 'block'
+
+                    })
+
+                    items_div.addEventListener("mouseleave", ()=>{
+                        cart_button.style.display = 'none'
+                    })
+
                     container.style.gridTemplateColumns = "auto auto auto"
                     
                     for_titles.style.marginLeft = "0px"
@@ -140,10 +179,86 @@ function getall_products(){
                     paragraph.style.display = 'none'
                     new_button.style.display = 'none'
                     second_price.style.display = 'block'
-                    
                     x=2
                 }
             })
+
+        }
+  
+        
+        function current_post(id){
+            let all_products = fetch(`https://dummyjson.com/products/${id}`)
+            .then(res => res.json())
+            .then((res => {
+                
+                let mainimage = document.querySelector(".main_image")
+                mainimage.src = res.images[0]
+
+                let image1 = document.querySelector(".image1")
+                let image2 = document.querySelector(".image2")
+                let image3 = document.querySelector(".image3")
+
+                image1.src = res.images[1]
+                image2.src = res.images[2]
+                image3.src = res.images[3]
+                
+
+                let div_stars = document.querySelector(".for_stars1")
+                
+                if (div_stars.childNodes.length===1){
+                    for(let i=0; i< Math.ceil(res.rating); i++){
+                        let star_image = document.createElement("img")
+                        star_image.classList.add("star_img")
+                        star_image.src = "./images/star-solid.svg"
+                        div_stars.append(star_image)
+                    
+   
+                }
+
+                }
+               
+              
+
+               let maintitle = document.querySelector(".main_title")
+               maintitle.innerHTML = res.title
+
+               let descrip = document.getElementById("decript")
+               descrip.innerHTML = res.description
+
+               let price1 = document.getElementById("discountedprice")
+               price1.innerHTML = `$${res.price}.00`
+
+               let price2 = document.getElementById("price1")
+               let first_variable = res.discountPercentage/100
+               let second_variable = 1 - first_variable
+               let third_variable = res.price/second_variable
+               price2.innerHTML = `$${Math.round(third_variable*100)/100}` 
+
+               let category = document.querySelector(".category1")
+               category.innerHTML = res.category
+
+               let popup = document.querySelector(".popup")
+
+               document.addEventListener('click', function(event) {
+                popup = document.querySelector('.popup');
+                
+                if (!popup.contains(event.target)) {
+                    
+                    popup.style.display = 'none';
+                }
+            });
+
+            popup.style.display = 'block';
+            
+
+               
+
+               
+
+            }))
+
+        
+
 
         }
         
@@ -154,24 +269,33 @@ function getall_products(){
         let second_number = 9
         let array = res.products.slice(first_number,second_number)
         let show_button = document.querySelector(".show_more")
-
+        
         show_button.addEventListener("click", ()=>{
+           
             first_number = second_number
             second_number = second_number + 9
             array = res.products.slice(first_number,second_number)
             array.forEach(element => {
                 get_items(element)
-
+    
             })
-            
-            
+                
+                
         })
+
         
+        
+       
+       
+
        
         
         array.forEach(element =>{
             get_items(element)
 
+            
+
+           
             
             
 
@@ -184,6 +308,7 @@ function getall_products(){
 
 
 getall_products()
+
 
 
 
